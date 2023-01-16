16ac8801c7f49834f65b6a6eb97ed67e187d7d03
hmmayna <119550894+hmmayna@users.noreply.github.com>
2023-01-15 18:19:06 -0600

    Update pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -1,3 +1,11 @@
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.lsky6wc.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(url)
+
+db = client.pytech
+
 lilly = {
     "student_id": "1007",
     "first_name": "Lilly",
