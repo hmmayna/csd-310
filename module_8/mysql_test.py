05fd6e9fae69c85f0acc083ba8ec53700fdec4fd
hmmayna <119550894+hmmayna@users.noreply.github.com>
2023-02-05 19:33:04 -0600

    Update mysql_test.py

--- module_8/mysql_test.py
+++ module_8/mysql_test.py
@@ -12,7 +12,7 @@ from mysql.connector import errorcode
 
 config = {
     "user": "pysports_user",
-    "password": "hmayna",
+    "password": "MySQL8IsGreat!",
     "host": "127.0.0.1",
     "database": "pysports",
     "raise_on_warnings": True
